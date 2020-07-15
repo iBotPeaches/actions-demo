f816c281ee032947b0d23bbb0d0dd10b3197da8e
Sergey Dolin <v-sedoli@microsoft.com>
2020-07-15 09:34:01 +0500

    Update Validate-SQLPowerShellTools.ps1

--- Validate-SQLPowerShellTools.ps1
+++ Validate-SQLPowerShellTools.ps1
@@ -1,15 +1,6 @@
 # Validate the installation
-$modules = Get-Module -Name SQLPS -ListAvailable
-Write-Host "The SQLPS Modules present are:"
-$modules | Select-Object Name,Version,Path | Format-Table
-
-# Validate the SQLserver PS module installation
-$modules = Get-Module -Name SQLServer -ListAvailable
-Write-Host "The SQLServer Modules present are:"
-$modules | Select-Object Name,Version,Path | Format-Table
-
 Describe 'Validate SQLPowerShellTools' {
-  Before {
+  BeforeAll {
     $env:PSModulePath = Get-SystemVariable "PSModulePath"
   }
   It 'The SQLPS module exists' {

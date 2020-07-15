362b319ff7f1a5b22eb2483f0cda7b6d489b7648
Sergey Dolin <v-sedoli@microsoft.com>
2020-07-15 11:29:04 +0500

    Update run-test.ps1

--- run-test.ps1
+++ run-test.ps1
@@ -130,3 +130,12 @@ If (Get-Command -Name Add-AssertionOperator -ErrorAction SilentlyContinue) {
 
 Invoke-PesterTests -TestFile "Validate-SQLPowerShellTools"
 Invoke-PesterTests -TestFile "Stack.Tests"
+Invoke-PesterTests -TestFile "Svn.Tests"
+Invoke-PesterTests -TestFile "Typescript.Tests"
+Invoke-PesterTests -TestFile "Vcpkg.Tests"
+Invoke-PesterTests -TestFile "VSWhereWDK.Tests"
+Invoke-PesterTests -TestFile "SSDTExtensions.Tests"
+Invoke-PesterTests -TestFile "WinAppDriver.Tests"
+Invoke-PesterTests -TestFile "WindowsUpdates.Tests"
+Invoke-PesterTests -TestFile "Wix.Tests"
+Invoke-PesterTests -TestFile "Zstd.Tests"

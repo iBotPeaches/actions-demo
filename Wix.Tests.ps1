c590e65ed8e05f5532140646965664a31f1a4b50
Sergey Dolin <v-sedoli@microsoft.com>
2020-07-15 13:27:10 +0500

    Update Wix.Tests.ps1

--- Wix.Tests.ps1
+++ Wix.Tests.ps1
@@ -1,1 +1,19 @@
 
+Describe "Wix" {
+    It "wix directory exists" {
+      $wad = "Windows Application Driver" 
+      if (${env:ProgramFiles(x86)})
+      {
+        $wadPath = "${env:ProgramFiles(x86)}\$wad"
+      }
+      else
+      {
+       $wadPath = "${env:ProgramFiles}\$wad"
+      }
+      Test-Path -Path $wadPath | Should Be $true
+    }
+    It "Developer Mode is enabled" {
+      $path = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock";
+      Get-ItemProperty -Path $path | Select-Object -ExpandProperty "AllowDevelopmentWithoutDevLicense" | Should -Be 1
+    }
+}

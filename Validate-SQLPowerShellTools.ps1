4d2143b11f3f67303df465a5aec95c724f3913b7
Sergey Dolin <v-sedoli@microsoft.com>
2020-07-15 10:16:53 +0500

    Update Validate-SQLPowerShellTools.ps1

--- Validate-SQLPowerShellTools.ps1
+++ Validate-SQLPowerShellTools.ps1
@@ -7,4 +7,8 @@ Describe 'Validate SQLPowerShellTools' {
      $modules = Get-Module -Name SQLPS -ListAvailable
      $modules.Count | Should -Be 1
   }
+  It 'The SQLServer module exists' {
+     $modules = Get-Module -Name SQLServer -ListAvailable
+     $modules.Count | Should -Be 1
+  }
 }

532476cab37182bbd5c8279bcddfcc4edf88ff8a
Sergey Dolin <v-sedoli@microsoft.com>
2020-07-16 11:07:46 +0500

    Update SSDTExtensions.Tests.ps1

--- SSDTExtensions.Tests.ps1
+++ SSDTExtensions.Tests.ps1
@@ -1,7 +1,7 @@
 
 Describe "SSDTExtensions" {
 #These extensions don't have any proper name in the state.packages.json file, only id is available, which can be found on extension marketplace download page
-    $ids = @{ id1 = "04a86fc2-dbd5-4222-848e-911638e487fe", id2 = "851E7A09-7B2B-4F06-A15D-BABFCB26B970", id3 = "717ad572-c4b7-435c-c166-c2969777f718" }
+    $ids = @{ id1 = "04a86fc2-dbd5-4222-848e-911638e487fe"; id2 = "851E7A09-7B2B-4F06-A15D-BABFCB26B970"; id3 = "717ad572-c4b7-435c-c166-c2969777f718" }
     It "Extensions id=${ids.id1}" {
     # TODO: replace with variables
       $version = Get-VSExtensionVersion -packageName $ids.id1

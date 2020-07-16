1ce002ebd0f1d3959602cb8f7baef514d149b907
Sergey Dolin <v-sedoli@microsoft.com>
2020-07-16 09:32:27 +0500

    Update SSDTExtensions.Tests.ps1

--- SSDTExtensions.Tests.ps1
+++ SSDTExtensions.Tests.ps1
@@ -5,15 +5,15 @@ Describe "SSDTExtensions" {
     $id2 = "851E7A09-7B2B-4F06-A15D-BABFCB26B970"
     $id3 = "717ad572-c4b7-435c-c166-c2969777f718"
     It "Extensions id=$id1" {
-      $version = Get-VSExtensionVersion -packageName "$id1"
+      $version = Get-VSExtensionVersion -packageName "04a86fc2-dbd5-4222-848e-911638e487fe"
       "$version" | Should -Not -Be ""
     }
     It "Extensions id=$id2" {
-      $version = Get-VSExtensionVersion -packageName "$id2"
+      $version = Get-VSExtensionVersion -packageName "851E7A09-7B2B-4F06-A15D-BABFCB26B970"
       "$version" | Should -Not -Be ""
     }
     It "Extensions id=$id3" {
-      $version = Get-VSExtensionVersion -packageName "$id3"
+      $version = Get-VSExtensionVersion -packageName "717ad572-c4b7-435c-c166-c2969777f718"
       "$version" | Should -Not -Be ""
     }
 }

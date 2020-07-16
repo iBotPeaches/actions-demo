f5ce5b861df9ce5dbad084806d2bdb0dd7750ed5
Sergey Dolin <v-sedoli@microsoft.com>
2020-07-16 11:03:28 +0500

    Update SSDTExtensions.Tests.ps1

--- SSDTExtensions.Tests.ps1
+++ SSDTExtensions.Tests.ps1
@@ -1,16 +1,14 @@
 
 Describe "SSDTExtensions" {
 #These extensions don't have any proper name in the state.packages.json file, only id is available, which can be found on extension marketplace download page
-    $id1 = "04a86fc2-dbd5-4222-848e-911638e487fe"
-    $id2 = "851E7A09-7B2B-4F06-A15D-BABFCB26B970"
-    $id3 = "717ad572-c4b7-435c-c166-c2969777f718"
-    It "Extensions id=$id1" {
+    $ids = @{ id1 = "04a86fc2-dbd5-4222-848e-911638e487fe", id2 = "851E7A09-7B2B-4F06-A15D-BABFCB26B970", id3 = "717ad572-c4b7-435c-c166-c2969777f718" }
+    It "Extensions id=${ids.id1}" {
     # TODO: replace with variables
-      $version = Get-VSExtensionVersion -packageName "04a86fc2-dbd5-4222-848e-911638e487fe"
+      $version = Get-VSExtensionVersion -packageName $ids.id1
       "$version" | Should -Not -Be ""
     }
-    It "Extensions id=$id3" {
-      $version = Get-VSExtensionVersion -packageName "717ad572-c4b7-435c-c166-c2969777f718"
+    It "Extensions id=${ids.id2}" {
+      $version = Get-VSExtensionVersion -packageName $ids.id2
       "$version" | Should -Not -Be ""
     }
   # TODO: not found

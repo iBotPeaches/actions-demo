7b4fd61374f67482371266dbdb6714c39324bea0
Sergey Dolin <v-sedoli@microsoft.com>
2020-07-27 14:09:06 +0500

    Update SSDTExtensions.Tests.ps1

--- SSDTExtensions.Tests.ps1
+++ SSDTExtensions.Tests.ps1
@@ -7,6 +7,10 @@ Describe "SSDTExtensions" {
 @{id = "717ad572-c4b7-435c-c166-c2969777f718"}
 )
  It "Extensions id=<id>" -TestCases $testExtenions {
+   param
+        (
+            [string] $id
+        )
    $version = Get-VSExtensionVersion -packageName $id
    $version | Should -Not -BeNullOrEmpty
  }

cea0b2741b19ef69e23a733d98562243cb826dd4
Sergey Dolin <sergey.dolin@gmail.com>
2020-12-11 13:34:55 +0500

    add Undefined symbols

--- tutorial.cxx
+++ tutorial.cxx
@@ -1,14 +1,20 @@
 #include <iostream>
+#include <string>
+#include <regex>
+
 #include "TutorialConfig.h"
 
+
 int main(int argc, char* argv[]) {
-  if (argc < 2) {
-    // report version
-    std::cout << argv[0] << " Version " << "." << std::endl;
-    std::cout << "Usage: " << argv[0] << " number" << std::endl;
-    return 1;
-  }
-// c++11
-  const double inputValue = std::stod(argv[1]);
-  return 0;
+    std::basic_string<char> s(argv[0]);
+    std::__cxx11::basic_string<char> s11(argv[0]);
+
+    std::basic_regex ninth ("\\bd\\w+", 0);     
+
+    std::string subject = "Duddy the duck";
+    std::string replacement = "yup";
+
+    std::cout << "Usage: " << s << s11 << regex_replace (subject, ninth, replacement) << std::endl;
+
+    return 0;
 }

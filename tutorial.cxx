8bf5a4d663f366e95c45863f57feb82c04890ddc
Sergey Dolin <sergey.dolin@gmail.com>
2020-12-11 13:00:35 +0500

    Add cmake sample

---
+++ tutorial.cxx
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "TutorialConfig.h"
+
+int main(int argc, char* argv[]) {
+  if (argc < 2) {
+    // report version
+    std::cout << argv[0] << " Version " << Tutorial_VERSION_MAJOR << "."
+              << Tutorial_VERSION_MINOR << std::endl;
+    std::cout << "Usage: " << argv[0] << " number" << std::endl;
+    return 1;
+  }
+// c++11
+  const double inputValue = std::stod(argv[1]);
+  return 0;
+}

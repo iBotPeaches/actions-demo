cdeaf7cb3493b82d965d0232fff46ce040729d02
Sergey Dolin <v-sedoli@micorosoft.com>
2021-03-16 13:41:00 +0500

    increase file

--- mksparse.c
+++ mksparse.c
@@ -7,7 +7,7 @@ void create_file_with_hole(void)
 {
     int fd = open("file.hole", O_WRONLY|O_TRUNC|O_CREAT, 0600);
     write(fd, "Hello", 5);
-    lseek(fd, 99988, SEEK_CUR); // Make a hole
+    lseek(fd, 999880, SEEK_CUR); // Make a hole
     write(fd, "Goodbye", 7);
     close(fd);
 }
@@ -16,9 +16,9 @@ void create_file_without_hole(void)
 {
     int fd = open("file.nohole", O_WRONLY|O_TRUNC|O_CREAT, 0600);
     write(fd, "Hello", 5);
-    char buf[99988];
-    memset(buf, 'a', 99988);
-    write(fd, buf, 99988); // Write lots of bytes
+    char buf[999880];
+    memset(buf, 'a', 999880);
+    write(fd, buf, 999880); // Write lots of bytes
     write(fd, "Goodbye", 7);
     close(fd);
 }

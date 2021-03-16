96fb8f153fd32a14f17368868ab314476786c1cb
Sergey Dolin <v-sedoli@micorosoft.com>
2021-03-16 14:11:51 +0500

    another build

--- mksparse.c
+++ mksparse.c
@@ -8,6 +8,7 @@ void create_file_with_hole(void)
     int fd = open("file.hole", O_WRONLY|O_TRUNC|O_CREAT, 0600);
     write(fd, "Hello", 5);
     lseek(fd, 999880, SEEK_CUR); // Make a hole
+    lseek(fd, 999880, SEEK_CUR); // Make a hole
     write(fd, "Goodbye", 7);
     close(fd);
 }
@@ -18,6 +19,7 @@ void create_file_without_hole(void)
     write(fd, "Hello", 5);
     char buf[999880];
     memset(buf, 'a', 999880);
+    write(fd, buf, 999880); // Write lots of bytes
     write(fd, buf, 999880); // Write lots of bytes
     write(fd, "Goodbye", 7);
     close(fd);
